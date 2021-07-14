80104999c36b8a6d3d7af84b00678709e104de33
Konstantin Novikov <nov.konstantin@gmail.com>
2021-07-14 11:45:41 +0200

    devops stuff

--- build-and-run.sh
+++ build-and-run.sh
@@ -7,3 +7,9 @@ minikube start
 eval $(minikube -p minikube docker-env)
 
 docker build -t k8s-angular ./src/clients/k8s-angular
+
+kubectl apply -f ./infra/k8s-angular/deployment.yml
+
+kubectl apply -f ./infra/k8s-angular/service.yml
+
+kubectl port-forward service/k8s-angular 8080:8080
